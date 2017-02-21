108906237122e613dbc15968f2b5b732c35c3d56
清山静水心 <caoyongfei030625@qq.com>
2017-02-21 10:33:12 +0800

    滚动设计1

--- src/klineScene/klineScene.js
+++ src/klineScene/klineScene.js
@@ -44,7 +44,7 @@ var KLineScene = SceneBase.extend(
 	middleHorizontalLineCount:11,	//在中间的横线的个数
 	
 	currentCandleIndex:0,		//当前显示的是第几个蜡烛，从0开始
-	CANDAL_DRAW_INTERVAL:100,		//每个K线相隔的时间
+	CANDAL_DRAW_INTERVAL:500,		//每个K线相隔的时间
 	currentCandleDrawInterval:null,	//当前的K线绘画间隔
 	drawCandleStoped:false,			//是否绘画停止了
 	
